63809036b08daa4f3a8ea0c73c0972d7f11bbdcf
Robledo <rpdrosa@inf.ufpel.edu.br>
2025-06-30 19:53:02 -0300

    teste

--- backend/controllers/LivroController.js
+++ backend/controllers/LivroController.js
@@ -3,8 +3,8 @@ const repo = require('../repositories/LivroRepository');
 
 class LivroController {
   listar() { return repo.getAll(); }
-  adicionar(t, a, y) {
-    return repo.add(new Livro(null, t, a, y));
+  adicionar(t, a, y, i) {
+    return repo.add(new Livro(null, t, a, y, i));
   }
   deletar(id) { repo.delete(id); }
 }
--- backend/routes/livros.js
+++ backend/routes/livros.js
@@ -4,7 +4,7 @@ const ctrl = require('../controllers/LivroController');
 
 router.get('/', (req, res) => res.json(ctrl.listar()));
 router.post('/', (req, res) => {
-  try { res.status(201).json(ctrl.adicionar(req.body.titulo, req.body.autor, req.body.ano)); }
+  try { res.status(201).json(ctrl.adicionar(req.body.titulo, req.body.autor, req.body.ano, req.body.ISBN)); }
   catch (e) { res.status(400).json({ erro: e.message }); }
 });
 router.delete('/:id', (req, res) => { ctrl.deletar(parseInt(req.params.id)); res.status(204).send(); });
